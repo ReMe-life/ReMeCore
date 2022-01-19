80ad20ea5684d9e3108237e42f034bcab8dc6265
charles-reme-dev <83959342+charles-reme-dev@users.noreply.github.com>
2022-01-19 19:28:31 +0000

    Add files via upload

---
+++ models/users.js
@@ -0,0 +1,14 @@
+var db = require('../db')
+var User = db.model('User',{
+  id: { type: String, required: true },
+  password: { type: String, required: true },
+  firstname: { type: String},
+  lastname: { type: String },
+  username: { type: String, required: true },
+  email: { type: String, required: true }
+})
+module.exports = User
+
+
+
+
