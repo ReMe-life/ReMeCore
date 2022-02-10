59bc456b31bdcb46fc60a9aa27ba0344bc7c1db9
charles-reme-dev <83959342+charles-reme-dev@users.noreply.github.com>
2022-02-10 11:20:48 +0000

    Create testscript-all.sh
    Simple test script

---
+++ testing/testscript-all.sh
@@ -0,0 +1,43 @@
+## 
+# Get  Start
+dev="https://devcore.remelife.com:8491"
+staging="https://devcore.remelife.com:8492"
+live="https://coresso.remelife.com:8000"
+
+
+URL=$dev
+## Get Verification Key
+
+curl -X GET $URL"/auth/verification_key
+
+
+i="02"
+j="02"
+firstname="First"$i$j
+lastname="Test-Last"$i$j
+username=$firstname$lastname"@surreydemo.co.uk"
+password="testing123"
+
+
+## Register User
+
+echo '{"firstname":"'$firstname'", "lastname":"'$lastname'","username":"'$username'","email":"'$username'","password":"'$password'"}'
+user=`curl -X POST -H "Content-Type: application/json" \
+--data '{"firstname":"'$firstname'", "lastname":"'$lastname'","username":"'$username'","email":"'$username'","password":"'$password'"}' \
+https://devcore.remelife.com:8491/auth/register`
+echo $user
+
+## Get UserId
+id=`echo $user | cut -d'"' -f4`
+echo $id
+
+curl -X GET  $URL/users/$id
+
+## Login
+
+
+
+userLogin=`curl -X POST -H "Content-Type: application/json" \
+--data '{"username":"'$username'","password":"'$password'"}' \
+$URL/auth/login`
+echo $userLogin
